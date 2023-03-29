7e43f19a885e7dad49c3c39eed3e46a073269d15
LokiVu <97932580+LokiVu@users.noreply.github.com>
2023-03-30 01:05:42 +0700

    update firebase

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,12 @@ import 'values/values.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: apiKey,
+          appId: '1:146548645926:android:4102d92335f5efb779c6e7',
+          messagingSenderId: '146548645926',
+          projectId: 'app-do-an-6f3b4'));
   runApp(const TheMovieApp());
 }
 
